3311a1c46decb77fb6049c6bc716c30c58fa2e15
Joppe Dechamps <95914160+JoppeDechamps@users.noreply.github.com>
2024-03-14 11:36:43 +0100

    Added S1 Create and Fill scripts to Samen

--- D5_QUERY/S1/Queries.sql
+++ D5_QUERY/S1/Queries.sql
@@ -1,9 +1,9 @@
 SELECT 'S1-A  : users' AS table_name,   (SELECT COUNT(*) FROM users) AS table_count   FROM DUAL UNION
-SELECT 'S1-B/X: smartphones',           (SELECT COUNT(*) FROM smartphones           ) FROM DUAL UNION
+SELECT 'S1-B  : smartphones',           (SELECT COUNT(*) FROM smartphones           ) FROM DUAL UNION
 SELECT 'S1-C  : websites',              (SELECT COUNT(*) FROM websites              ) FROM DUAL UNION
 SELECT 'S1-D  : reviews',               (SELECT COUNT(*) FROM reviews               ) FROM DUAL UNION
-SELECT 'S2-  : brands',                 (SELECT COUNT(*) FROM brands                ) FROM DUAL UNION
-SELECT 'S2-  : brand_stores',           (SELECT COUNT(*) FROM brand_stores          ) FROM DUAL UNION
-SELECT 'S2-  : sales',                  (SELECT COUNT(*) FROM sales                 ) FROM DUAL UNION
-SELECT 'S2-  : promotions',             (SELECT COUNT(*) FROM promotions            ) FROM DUAL UNION
+SELECT 'S2-X  : brands',                (SELECT COUNT(*) FROM brands                ) FROM DUAL UNION
+SELECT 'S2-Y  : brand_stores',          (SELECT COUNT(*) FROM brand_stores          ) FROM DUAL UNION
+SELECT 'S2-Z  : sales',                 (SELECT COUNT(*) FROM sales                 ) FROM DUAL UNION
+SELECT 'S2-W  : promotions',            (SELECT COUNT(*) FROM promotions            ) FROM DUAL UNION
 SELECT 'S2_  : addresses',              (SELECT COUNT(*) FROM addresses             ) FROM DUAL;
