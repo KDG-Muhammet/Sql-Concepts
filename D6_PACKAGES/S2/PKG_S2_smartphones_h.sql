28a3f81942b21473f0beb4563f4c80dca389c750
muhammet murat <muhammet.murat@student.kdg.be>
2024-05-21 01:22:27 +0200

    code geordend

--- D6_PACKAGES/S2/PKG_S2_smartphones_h.sql
+++ D6_PACKAGES/S2/PKG_S2_smartphones_h.sql
@@ -4,13 +4,17 @@ CREATE OR REPLACE PACKAGE PKG_S2_smartphones AS
 
     PROCEDURE bewijs_milestone_M5_S2;
 
-
     PROCEDURE generate_addresses(p_count IN NUMBER);
     PROCEDURE generate_brands(p_count IN NUMBER);
-    PROCEDURE generateBrandStores(p_num_stores_per_brand IN NUMBER);
+    PROCEDURE generate_brands_stores(p_num_stores_per_brand IN NUMBER);
     PROCEDURE generate_promotions(p_count IN NUMBER);
     PROCEDURE generate_sales(s_rows_per_store IN NUMBER);
 
+    PROCEDURE generate_addresses_bulk(p_count IN NUMBER);
+    PROCEDURE generate_brands_bulk(p_count IN NUMBER);
+    PROCEDURE generate_brand_stores_bulk(p_num_stores_per_brand IN NUMBER);
+    PROCEDURE generate_promotions_bulk(p_count IN NUMBER);
+    PROCEDURE generate_sales_bulk(s_rows_per_store IN NUMBER);
 
 END PKG_S2_smartphones;
 /
