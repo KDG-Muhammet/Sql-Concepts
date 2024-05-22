06275d8fb10d4406203297843b4e637a45a9b568
muhammet murat <muhammet.murat@student.kdg.be>
2024-05-23 01:38:52 +0200

    m8 explain plan + view

--- D7_OPTIMALISATION/S2/M8_S2.sql
+++ D7_OPTIMALISATION/S2/M8_S2.sql
@@ -1,19 +1,28 @@
 ------------------------------------ milestone 8 --------------------------------
+-- ik wil een overzicht zien van het aantal sales per promotion van een phone
 
--- ik wil een overzicht zien van het aantal sales per promotion per store van een phone
-EXPLAIN PLAN FOR
-SELECT Sm.NAME, p.NAME, COUNT(SALE_ID),STORE_ID
-from SALES S
-         join SMARTPHONES Sm on Sm.PHONE_ID = S.PHONE_ID
-         join PROJECT.PROMOTIONS P on P.PROMOTION_ID = S.PROMOTION_ID
-where Sm.NAME = 'Samsung Note 7'
-group by sm.NAME, P.NAME ,STORE_ID;
+--------Originele query
+SELECT S.STORE_ID,
+       Sm.NAME                                                                        AS SMARTPHONE_NAME,
+       P.NAME                                                                         AS PROMOTION_NAME,
+       SUM(S.DUE_DATES)                                                               AS TOTAL_SALE_VALUE,
+       COUNT(S.SALE_ID)                                                               AS TOTAL_SALES,
+       SUM(S.DUE_DATES) OVER (PARTITION BY S.STORE_ID ORDER BY SUM(S.DUE_DATES) DESC) AS CUMULATIVE_SALES,
+       RANK() OVER (PARTITION BY S.STORE_ID ORDER BY SUM(S.DUE_DATES) DESC)           AS PROMOTION_RANK
+FROM SALES S
+         JOIN
+     SMARTPHONES Sm ON Sm.PHONE_ID = S.PHONE_ID
+         JOIN
+     PROMOTIONS P ON P.PROMOTION_ID = S.PROMOTION_ID
+WHERE Sm.NAME = 'Samsung Note 7'
+GROUP BY S.STORE_ID, Sm.NAME, P.NAME
+ORDER BY S.STORE_ID, PROMOTION_RANK;
 
--- Bekijk het explain plan
-SELECT * FROM TABLE(DBMS_XPLAN.DISPLAY());
-
+------- Bereken de statistieken voor de tabel
 BEGIN
     DBMS_STATS.GATHER_TABLE_STATS('PROJECT', 'SALES');
+    DBMS_STATS.GATHER_TABLE_STATS('PROJECT', 'SMARTPHONES');
+    DBMS_STATS.GATHER_TABLE_STATS('PROJECT', 'PROMOTIONS');
 end;
 
 select segment_name,
@@ -25,23 +34,78 @@ where segment_name = 'SALES'
 group by segment_name, segment_type;
 -- output voor sales met 633005 is 40mb
 
+-------- Verkrijgen van het explain plan
+EXPLAIN PLAN FOR
+SELECT Sm.NAME AS smartphone_name, P.NAME AS promotion_name, COUNT(SALE_ID) AS sale_count
+FROM SALES S
+         JOIN SMARTPHONES Sm ON Sm.PHONE_ID = S.PHONE_ID
+         JOIN PROMOTIONS P ON P.PROMOTION_ID = S.PROMOTION_ID
+WHERE Sm.NAME = 'Samsung Note 7'
+GROUP BY Sm.NAME, P.NAME;
+
+-------- Bekijk het explain plan
+SELECT *
+FROM TABLE (DBMS_XPLAN.DISPLAY());
+
+--------- Materialized View
 
 GRANT CREATE TABLE TO PROJECT;
--- Materialized View
+DROP MATERIALIZED VIEW mv1;
 CREATE MATERIALIZED VIEW
     mv1
-            BUILD IMMEDIATE
-    REFRESH ON DEMAND AS
-SELECT Sm.NAME, p.NAME, COUNT(SALE_ID),STORE_ID
-from SALES S
-         join SMARTPHONES Sm on Sm.PHONE_ID = S.PHONE_ID
-         join PROJECT.PROMOTIONS P on P.PROMOTION_ID = S.PROMOTION_ID
-where Sm.NAME = 'Samsung Note 7'
-group by sm.NAME, P.NAME ,STORE_ID;
+AS
+SELECT Sm.NAME AS smartphone_name, P.NAME AS promotion_name, COUNT(SALE_ID) AS sale_count
+FROM SALES S
+         JOIN SMARTPHONES Sm ON Sm.PHONE_ID = S.PHONE_ID
+         JOIN PROMOTIONS P ON P.PROMOTION_ID = S.PROMOTION_ID
+WHERE Sm.NAME = 'Samsung Note 7'
+GROUP BY Sm.NAME, P.NAME;
 
--- Query opnieuw uitvoeren met de materialized view
+------- Bereken de statistieken voor de tabel
+BEGIN
+    DBMS_STATS.GATHER_TABLE_STATS('PROJECT', 'mv1');
+end;
+
+select segment_name,
+       segment_type,
+       sum(bytes / 1024 / 1024)      MB,
+       (select COUNT(*) FROM mv1) as table_count
+from dba_segments
+where segment_name = 'MV1'
+group by segment_name, segment_type;
+
+------- Query opnieuw uitvoeren met de materialized view
+SELECT smartphone_name, promotion_name, sale_count
+FROM mv1
+WHERE smartphone_name = 'Samsung Note 7';
+
+
+------- Verkrijgen van het explain plan met mv
 EXPLAIN PLAN FOR
-SELECT * FROM PROJECT.MV1;
+SELECT smartphone_name, promotion_name, sale_count
+FROM MV1
+WHERE smartphone_name = 'Samsung Note 7';
 
--- Bekijk het explain plan
-SELECT * FROM TABLE(DBMS_XPLAN.DISPLAY());
+------- Bekijk het explain plan
+SELECT *
+FROM TABLE (DBMS_XPLAN.DISPLAY());
+
+-----------negatieve gevolgen----------
+
+-- Voeg nieuwe gegevens toe aan de tabellen
+INSERT INTO SALES (DUE_DATES, PHONE_ID, PROMOTION_ID, STORE_ID, NAME, SALE_DATE)
+VALUES ( SYSDATE, 3, 1003, 1, 'Test view', SYSDATE);
+
+
+-- Controleer of de nieuwe gegevens zichtbaar zijn in de materialized view
+SELECT smartphone_name, promotion_name, sale_count
+FROM mv1;
+
+-- Vernieuw de materialized view en controleer opnieuw
+BEGIN
+    DBMS_MVIEW.REFRESH('MV1', 'C');
+END;
+
+SELECT smartphone_name, promotion_name, sale_count
+FROM mv1;
+
